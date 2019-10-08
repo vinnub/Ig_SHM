eff95786953a543763346b61e3546149e5110250
Vinnu Bhardwaj <vbhardwaj@bloom.ucsd.edu>
2019-10-07 17:18:03 -0700

    added functionalities to compute null distrbn, conf int, and observed test statistics

--- utils.py
+++ utils.py
@@ -4,12 +4,13 @@ import seaborn as sns
 import numpy as np
 import networkx as nx
 import pyvolve
+from joblib import Parallel, delayed
 
 def get_delta_matrix(filename, sheetname):
     '''
     Function to extract the delta matrix from a sheet of the excel file 'aadelta_matrices.xlsx'.
     Note that this function is very specific to the way data is structured in this file. It will not work correclty for other files. 
-    '''
+    ''' 
     def _extract_aa_code(col_name):
         return col_name.split('(')[1].split(')')[0]
     AA_property = pd.read_excel(filename, sheet_name=sheetname).iloc[21:,:]
@@ -48,7 +49,7 @@ class delta_matrices:
     def plot(self):
         fig, axs = plt.subplots(2, 2, figsize = (16,10))
         axs = axs.ravel()
-        for i, a_property in enumerate(['volume', 'composition','aromaticity', 'polarity']):
+        for i, a_property in enumerate(['volume', 'polarity', 'composition', 'aromaticity']):
             delta_df = getattr(self,a_property)
             sns.heatmap(np.tril(delta_df),cmap = 'GnBu',vmax = 1., square=True, cbar_kws={"shrink": .5}, ax = axs[i] );
             axs[i].set_xticklabels(labels= delta_df.columns)
@@ -105,7 +106,7 @@ class clonal_graph:
 
         def get_pyvolve_phylogeny_from_nxgraph(G, root_seq):
             tree_newick = networkx_to_newick(G)
-            scale_tree = 0.3#1/len(root_seq)
+            scale_tree = 1/len(root_seq)
             tree = pyvolve.read_tree(tree =tree_newick.replace('a', 'a:'+str(scale_tree)))
             return tree 
         
@@ -114,7 +115,7 @@ class clonal_graph:
         self.pyvolve_phylogeny = get_pyvolve_phylogeny_from_nxgraph(self.nx_graph, self.root_sequence)
         
         
-    def sims_to_null_dists(self, a_delta_matrices_instance):
+    def sims_to_null_dists_serially(self, a_delta_matrices_instance):
         assert len(self.simulations) !=0, "No simulation data present in the clonal_graph object"
         test_stats_dfs = []
         for i, sim_seqs in enumerate(self.simulations):
@@ -122,16 +123,62 @@ class clonal_graph:
             test_stats_dfs.append(a_df)
 
         return pd.concat(test_stats_dfs, axis = 1)
+    
+    def sims_to_null_dists_parallel(self, a_delta_matrices_instance, cpus, return_mean_stats = False ):
+        assert len(self.simulations) !=0, "No simulation data present in the clonal_graph object"
+        test_stats_dfs= Parallel(n_jobs=cpus)(delayed(get_test_stats)(sim_seqs, self.edges, a_delta_matrices_instance, return_mean_stats) \
+                           for sim_seqs in self.simulations)
+        return pd.concat(test_stats_dfs, axis = 1)
 
-        
-def get_test_stats(simulated_seqs, edge_list, a_delta_matrices_instance):
+    def get_observed_stats(self, a_delta_matrices_instance, return_mean_stats = False):
+        shm_df =  pd.read_csv(self.shm_file, sep = '\t')
+        add_deltas = shm_df.apply(lambda x : x['Multiplicity'] *\
+                                    pd.Series(a_delta_matrices_instance.deltas_between_AA(x['Src_AA'], x['Dst_AA'])), axis = 1)
+        shm_df_with_deltas = pd.concat([shm_df, add_deltas], axis=1)
+        observed_stats = shm_df_with_deltas.groupby(['Region', 'Position'])[['Multiplicity',
+                                                                             'volume', 'polarity', 
+                                                                             'composition', 'aromaticity']].sum()
+        if not return_mean_stats:
+            return observed_stats
+        else:
+            return observed_stats.apply(lambda x : x/x['Multiplicity'], axis = 1)
+
+
+    
+def get_test_stats(simulated_seqs, edge_list, a_delta_matrices_instance, return_mean_stats ):
+    
     delta_dfs = []
     for src, dst in edge_list:
         delta_dfs.append(a_delta_matrices_instance.deltas_between_seqs(simulated_seqs[src], simulated_seqs[dst]))
-    return sum(delta_dfs)
+    sum_stats = sum(delta_dfs)
+    if not return_mean_stats: return sum_stats
+    
+    mutations = get_num_mutations(simulated_seqs, edge_list)
+    multiple_mutations = mutations[mutations>1]
+    if len(multiple_mutations) == 0: return sum_stats
+    
+    non_zero_mean_stats = sum_stats
+    for index, value in multiple_mutations.items():
+        non_zero_mean_stats[index] =  (non_zero_mean_stats[index])/value
+    
+    return non_zero_mean_stats
         
-        
-        
+    
+def get_num_mutations(seqs_dict, edge_list):
+    
+    def diff_between_seqs(seq1, seq2):
+        assert len(seq1) == len(seq2), "Lengths of sequences must be the same"
+        diff_series = pd.Series(index=range(len(seq1)))
+        for i, _ in enumerate(seq1):
+            diff_series[i] =  (seq1[i] != seq2[i])
+        return diff_series.astype(int)
+
+    mutations_bw_two_seqs = []
+    for src, dst in edge_list:
+        mutations_bw_two_seqs.append(diff_between_seqs(seqs_dict[src], seqs_dict[dst]))
+    return sum(mutations_bw_two_seqs)
+
+
 def networkx_to_newick(nx_graph):
     def bfs_edge_lst(graph, n):
         return list(nx.bfs_edges(graph, n))
@@ -177,3 +224,4 @@ def plot_nx_graph(G):
     nx.draw_networkx_labels(G, pos, font_color = 'black', font_size = 8)
     nx.draw_networkx_edges(G, pos, edgelist=G.edges, edge_color='blue', arrows=True)
     plt.show()
+    
